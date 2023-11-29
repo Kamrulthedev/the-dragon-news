f99260fb259e01dc2551b7ad5f7b0e910459dbde
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-30 00:51:38 +0600

    update-config-commit

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,14 +5,17 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBg3xXzjfLzHzNbz4p4up5sxG4zAnABVeI",
-  authDomain: "the-dragon-news-4b74f.firebaseapp.com",
-  projectId: "the-dragon-news-4b74f",
-  storageBucket: "the-dragon-news-4b74f.appspot.com",
-  messagingSenderId: "767204733980",
-  appId: "1:767204733980:web:966a2d9538e3589a441bd9"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
