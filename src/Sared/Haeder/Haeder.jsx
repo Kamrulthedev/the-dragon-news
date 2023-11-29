db3fdb23bee75ad27c69efe9931e36f07a88beca
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-29 09:10:22 +0600

    date-commit

--- src/Sared/Haeder/Haeder.jsx
+++ src/Sared/Haeder/Haeder.jsx
@@ -8,7 +8,7 @@ const Haeder = () => {
        <div className='flex  justify-center mt-8'>
        <img  src={logo} alt="" />
        </div>
-          <div>
+          <div className='text-center'>
             <p> Journalism Without Fear or Favour</p>
              <h2 className='text-xl '>{moment().format("dddd, MMMM D, YYYY")}</h2>
           </div>
