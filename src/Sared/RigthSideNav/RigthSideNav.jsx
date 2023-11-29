27645f3015a3dfed510046b1cd8506118d345d3c
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-29 11:36:59 +0600

    stroung-pages-commit

--- src/Sared/RigthSideNav/RigthSideNav.jsx
+++ src/Sared/RigthSideNav/RigthSideNav.jsx
@@ -3,6 +3,7 @@ import { AiFillTwitterCircle } from "react-icons/ai";
      import qzone1 from "../../assets/qZone1.png";
      import qzone2 from "../../assets/qZone2.png";
      import qzone3 from "../../assets/qZone3.png";
+     import bg1 from "../../assets/bg.png";
 
 const RigthSideNav = () => {
     return (
@@ -36,12 +37,21 @@ const RigthSideNav = () => {
             </div>
             {/* Q Zone  */}
 
-            <div className="p-4 mb-4 ">
+            <div className="p-4 mb-4 bg-gray-100	">
                 <h2 className="text-2xl font-bold mb-6">Q-Zone</h2>
              <img className="mb-3" src={qzone1} alt="" />
              <img className="mb-3" src={qzone2} alt="" />
              <img src={qzone3} alt="" />
             
+            </div>
+            <div className="bg-cover p-12 bg-center" style={{ backgroundImage: `url(${bg1}` }}>
+                <h1 className="text-4xl mb-4 font-bold text-center text-white ">Create an 
+                   <br  /> Amazing
+                   <br /> Newspaper</h1>
+                   <p className="text-center mb-4 text-white">Discover thousands of <br /> options, easy to <br /> customize layouts, one- <br /> click to import demo <br /> and much more.</p>
+                  <div className="flex justify-center">
+                  <button className="btn btn-secondary mb-8">Learn More</button>
+                  </div>
             </div>
         </div>
     );
